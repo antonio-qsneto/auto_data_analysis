63780ab023661d8e1288dfa53c2f37c18aabeb49
antonio-qsneto <prof.antonioqsneto@gmail.com>
2025-10-08 17:38:40 -0300

    feat: change directory structure

--- frontend/src/routes/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { getAccessToken } from "../utils/auth";
-
-export default function PrivateRoute({ children }) {
-  const token = getAccessToken();
-  return token ? children : <Navigate to="/login" replace />;
-}
