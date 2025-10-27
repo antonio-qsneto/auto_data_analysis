ffd89299bdf17bbe6a57ef33c47ed07fc1006336
antonio-qsneto <prof.antonioqsneto@gmail.com>
2025-10-27 17:59:55 -0300

    fix: celery name app fixed

--- backend/auto_data_analysis/celery.py
+++ backend/auto_data_analysis/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "auto_data_analysis.settings")
 
-app = Celery("auto_data_analysis")
+app = Celery("dashboard")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
