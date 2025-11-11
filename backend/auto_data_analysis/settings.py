eb56b5735207c44d580fc532f804616fafc56d5b
antonio-qsneto <prof.antonioqsneto@gmail.com>
2025-11-11 19:51:10 -0300

    feat: add cache redis explicitly

--- backend/auto_data_analysis/settings.py
+++ backend/auto_data_analysis/settings.py
@@ -281,3 +281,12 @@ CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "UTC"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 60 * 20  # 20 minutos por segurança
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": os.getenv("REDIS_URL", "redis://redis:6379/0"),
+        "TIMEOUT": 60 * 60,  # 1 hora de persistência
+    }
+}
